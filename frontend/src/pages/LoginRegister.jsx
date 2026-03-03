155c0dc36fb565ddf93f55f84cf045f2121f3dba
RayenBt7 <151744296+RayenBt7@users.noreply.github.com>
2026-03-03 01:07:45 +0100

    Refactor import statement for useState

--- frontend/src/pages/LoginRegister.jsx
+++ frontend/src/pages/LoginRegister.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
